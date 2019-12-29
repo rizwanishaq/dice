beac7e34a327ece954e989d7e5b50eecd647ca8d
Rizwan Ishaq <rizwanishaq@gmail.com>
2019-12-29 19:29:38 +0100

    Updates

---
+++ precache-manifest.8874d24b6babeb7afda90997b080a354.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00681b1e5b7fa69753f35b1897d2e8b1",
+    "url": "/dice/index.html"
+  },
+  {
+    "revision": "c6ff5a1d0c240eb0a098",
+    "url": "/dice/static/css/main.28ada66a.chunk.css"
+  },
+  {
+    "revision": "4aac3d5868e1fe6d5758",
+    "url": "/dice/static/js/2.65992982.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dice/static/js/2.65992982.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c6ff5a1d0c240eb0a098",
+    "url": "/dice/static/js/main.e682c088.chunk.js"
+  },
+  {
+    "revision": "bd2867f10c0229a34307",
+    "url": "/dice/static/js/runtime-main.38fa6f00.js"
+  }
+]);
